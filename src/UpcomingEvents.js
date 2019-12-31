068aeee6723ffd4772850f7053950d2a1b8cc8e6
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2019-12-31 12:41:11 -0500

    redesign upcoming events

--- src/UpcomingEvents.js
+++ src/UpcomingEvents.js
@@ -18,38 +18,51 @@ class UpcomingEvent extends Component {
 
     render() {
         return (
-            <li className="justify-content-between align-items-center">
-            {/* <figure><a href="#"><img src={require('./images/u-1.jpg')} alt="" /></a></figure> */}
+        //     <li className="justify-content-between align-items-center">
+        //     {/* <figure><a href="#"><img src={require('./images/u-1.jpg')} alt="" /></a></figure> */}
     
 
-            <div className="entry-content" 
-                                            style={{ 
-                                                overflow: 'hidden', 
-                                                marginBottom: '20px'
-                                                }}>
+        //     <div className="entry-content" 
+        //                                     style={{ 
+        //                                         overflow: 'hidden', 
+        //                                         marginBottom: '20px'
+        //                                         }}>
                 
-               <h6 
-                    className="entry-title cal-event" 
-                    onClick={this.openDescription}
-                    style={{ cursor: 'pointer'
-                                 }}                    
+        //        <h6 
+        //             className="entry-title cal-event" 
+        //             onClick={this.openDescription}
+        //             style={{ cursor: 'pointer'
+        //                          }}                    
                 
-                >{this.props.heading}</h6>
+        //         >{this.props.heading}</h6>
 
-            <p>
-                {this.props.shortDate}
-            </p>
+        //     <p>
+        //         {this.props.shortDate}
+        //     </p>
 
-                <div className="post-metas d-flex flex-wrap align-items-center">
-                    {/* <span className="posted-date"><a href="#">{upcoming.shortDate}</a></span> */}
-                    {/* <span className="event-location"><a href="#">Event Details</a></span> */}
-                </div>
+        //         <div className="post-metas d-flex flex-wrap align-items-center">
 
-            { this.state.open && <p className="single-post-text single-post-preview" dangerouslySetInnerHTML= {{ __html: DOMPurify.sanitize(this.props.text) }} ></p> }                         
+        //         </div>
+
+        //     { this.state.open && <p className="single-post-text single-post-preview" dangerouslySetInnerHTML= {{ __html: DOMPurify.sanitize(this.props.text) }} ></p> }                         
                 
-            </div>
+        //     </div>
  
-        </li>
+        // </li>
+
+
+// 
+<li className="d-flex flex-wrap align-items-center" onClick={this.openDescription} style={{ cursor: 'pointer' }}>
+
+<figure className="d-flex justify-content-center"><img src={require('./images/LCLO-Icon.png')} style={{ width: '60%' }} alt="" /></figure>
+
+<div className="entry-content d-flex align-items-center">
+    <a className="d-flex"><h3 className="entry-title" style={{ color: '#ff4800', marginRight: '5px' }}>{ this.props.shortDate + ' '}</h3> <h3 className="entry-title">{this.props.heading}</h3></a>
+</div>
+
+{ this.state.open && <div className="single-post-text single-post-preview" style={{ padding: '10px', width: '100%' }}> <p dangerouslySetInnerHTML= {{ __html: DOMPurify.sanitize(this.props.text) }} style={{ padding: '10px', width: '100%', border: '1px solid #E0E0E0' }}></p></div> }                         
+
+</li>
         )
     }
 }
@@ -83,15 +96,15 @@ class UpcomingEvents extends Component {
       )  
 
         return (
-            <div className="upcoming-events">
+            <div className="popular-posts">
             {this.state.upcoming.length !== 0 && <h2>Upcoming Events</h2>}
-            <div>
+            
             <ul className="p-0" style={{ listStyleType: 'none' }}>
 
                 <UpcomingList />
 
             </ul>
-            </div>
+            
 
         </div>
         )
